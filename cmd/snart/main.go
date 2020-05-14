4fed56861eb96d07ac3deab6408d2fc031c168d7
Terra Brown <superloach42@gmail.com>
2020-05-14 12:28:31 -0400

    monorepo!!!

---
+++ cmd/snart/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/namsral/flag"
+	"github.com/superloach/minori"
+
+	"github.com/go-snart/snart/bot"
+	"github.com/go-snart/snart/db"
+
+	_ "github.com/go-snart/plugin-admin"
+	_ "github.com/go-snart/plugin-help"
+)
+
+var (
+	debug = flag.Bool("debug", false, "print debug messages")
+
+	dbhost = flag.String("dbhost", "localhost", "rethinkdb host")
+	dbport = flag.Int("dbport", 28015, "rethinkdb port")
+	dbuser = flag.String("dbuser", "admin", "rethinkdb username")
+	dbpass = flag.String("dbpass", "", "rethinkdb password")
+)
+
+var Log = minori.GetLogger("snart")
+
+func main() {
+	_f := "main"
+	flag.Parse()
+
+	if *debug {
+		minori.Level = minori.DEBUG
+		Log.Debug(_f, "debug mode")
+	} else {
+		minori.Level = minori.INFO
+	}
+
+	d := &db.DB{
+		Host: *dbhost,
+		Port: *dbport,
+		User: *dbuser,
+		Pass: *dbpass,
+	}
+
+	// make bot
+	b, err := bot.MkBot(d)
+	if err != nil {
+		err = fmt.Errorf("mkbot %#v: %w", d, err)
+		Log.Fatal(_f, err)
+	}
+
+	// run the bot
+	err = b.Start()
+	if err != nil {
+		err = fmt.Errorf("start: %w", err)
+		Log.Fatal(_f, err)
+	}
+
+	Log.Info(_f, "bye!")
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module github.com/go-snart/snart
 go 1.14
 
 require (
-	github.com/go-snart/bot v0.0.0-20200514160339-e29a19274c57
-	github.com/go-snart/db v0.0.0-20200513150217-b5c6656488e6
+	github.com/bwmarrin/discordgo v0.20.3
+	github.com/dlclark/regexp2 v1.2.0
 	github.com/go-snart/plugin-admin v0.0.0-20200514160444-d36c4c4c63fa
 	github.com/go-snart/plugin-help v0.0.0-20200514160507-15a07a8b1cd8
 	github.com/namsral/flag v1.7.4-pre
 	github.com/superloach/minori v0.0.0-20200401022729-31f6f02808bc
+	gopkg.in/rethinkdb/rethinkdb-go.v6 v6.2.1
 )

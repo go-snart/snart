7af189315d1323414275fa4833fddbb898ba7d62
Terra Brown <superloach42@gmail.com>
2020-06-04 15:57:12 -0400

    updates, queryer

---
+++ db/queryer.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/go-snart/snart/route"
+	r "gopkg.in/rethinkdb/rethinkdb-go.v6"
+)
+
+func (d *DB) Queryer(getq func(*route.Ctx) (r.Term, error)) func(*route.Ctx) error {
+	_f := "Queryer"
+
+	return func(ctx *route.Ctx) error {
+		rep := ctx.Reply()
+
+		q, err := getq(ctx)
+		if err != nil {
+			err = fmt.Errorf("apply f to ctx: %w", err)
+
+			rep.Content = err.Error()
+			rep.Send()
+
+			Log.Error(_f, err)
+
+			return err
+		}
+
+		tmp := make([]interface{}, 0)
+		err = q.ReadAll(tmp, d)
+		if err != nil {
+			err = fmt.Errorf("readall tmp %s: %w", q, err)
+
+			rep.Content = err.Error()
+			rep.Send()
+
+			Log.Error(_f, err)
+
+			return err
+		}
+
+		cont, err := json.MarshalIndent(tmp, "", "\t")
+		if err != nil {
+			err = fmt.Errorf("marshal cont: %w", q, err)
+
+			rep.Content = err.Error()
+			rep.Send()
+
+			Log.Error(_f, err)
+
+			return err
+		}
+
+		rep.Content = "```json\n"
+		for _, line := range strings.Split(string(cont), "\n") {
+			rep.Content += line + "\n"
+			if len(rep.Content) > 1950 {
+				rep.Content += "```"
+				err = rep.Send()
+				if err != nil {
+					return err
+				}
+				rep.Content = "```json\n"
+			}
+		}
+		return rep.Send()
+	}
+}
